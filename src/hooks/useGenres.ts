9bdad6a4fb2804ed7a3fef46c3d83347c0197163
Mostafa Eltantawy <eltantawymostafa2@gmail.com>
2023-10-17 18:00:43 +0300

    implement infinite scroll

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -1,7 +1,6 @@
 import { useQuery } from '@tanstack/react-query';
 import genres from '../data/genres';
 import APIClient from '../services/api-client';
-import { FetchResponse } from '../services/api-client';
 
 const apiClient = new APIClient<Genre>('/genres');
 export interface Genre {
@@ -15,7 +14,7 @@ const useGenres = () =>
     queryKey: ['genres'],
     queryFn: apiClient.getAll,
     staleTime: 24 * 60 * 60 * 1000,
-    initialData: { count: genres.length, results: genres },
+    initialData: { count: genres.length, results: genres, next: null },
   });
 
 export default useGenres;
