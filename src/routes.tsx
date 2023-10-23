2774056f32f4180438f70ed8f6271c4468e6bc01
Mostafa Eltantawy <eltantawymostafa2@gmail.com>
2023-10-23 17:43:04 +0300

    Handle errors

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,4 +1,5 @@
 import { createBrowserRouter } from 'react-router-dom';
+import ErrorPage from './pages/ErrorPage';
 import GameDetailPage from './pages/GameDetailPage';
 import HomePage from './pages/HomePage';
 import Layout from './pages/Layout';
@@ -6,7 +7,8 @@ import Layout from './pages/Layout';
 const router = createBrowserRouter([
   {
     path: '/',
-    element: <Layout></Layout>,
+    element: <Layout />,
+    errorElement: <ErrorPage />,
     children: [
       { index: true, element: <HomePage /> },
       { path: 'games/:id', element: <GameDetailPage /> },
