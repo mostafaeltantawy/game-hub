02f7f67f916acd52ea9996bed296d7ea5cf45d27
Mostafa Eltantawy <eltantawymostafa2@gmail.com>
2023-10-23 18:48:58 +0300

    Build game attributes

--- src/entities/Game.ts
+++ src/entities/Game.ts
@@ -1,11 +1,15 @@
+import { Genre } from './Genre';
 import { Platform } from './Platform';
+import { publisher } from './Publisher';
 
 export interface Game {
   id: number;
   name: string;
+  genres: Genre[];
   background_image: string;
   parent_platforms: { platform: Platform }[];
   metacritic: number;
+  publishers: publisher[];
   rating_top: number;
   slug: string;
   description_raw: string;
